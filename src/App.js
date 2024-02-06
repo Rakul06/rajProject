ecad08beb0cf0c436b13db8e1f2e41674e455bd0
rakul_b <rakul_b@solartis.com>
2024-02-06 18:40:19 +0530

    added imges

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
+import logo from './img/Daco_4935300.png'
+import alagarWithHorse from './img/MicrosoftTeams-image (1).png'
+import sView from './img/MicrosoftTeams-image (2).png'
+import model from './img/MicrosoftTeams-image (3).png'
+import gopuram from './img/MicrosoftTeams-image (4).png'
+import tnlogo from './img/MicrosoftTeams-image (5).png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+      <div className="main-container">
+        <div className='title'>
+          <div>
+            <h1>THE</h1>
+          </div>
+          <div>
+            <img src={logo}alt="" />
+          </div>
+          <div>
+            <h1>HINDU</h1>
+          </div>
+        </div>
+        <div className='img-container'>
+          <div>
+            jjsjj
+          </div>
+          <div>
+            jjsjj
+          </div>
+        </div>
+      </div>
+      
   );
 }
 
